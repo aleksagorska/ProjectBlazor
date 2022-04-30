d7ed15df3cf8893e147dfe90cdcf773a990f56db
aleksagorska <aleksagorska98@gmail.com>
2022-04-30 14:35:32 +0200

    final

---
+++ Client/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? PathToImg { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Position { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+
+    }
+}
